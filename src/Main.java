24856025b60304ea45096c9ea77f8cfb65196876
Jan Sváček <jan@svacek.eu>
2023-05-30 12:01:05 +0200

    Initial commit - 30.5 ODB KURZ

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //RGB
+        //System.out.println("R:");
+        int r = sc.nextInt();
+        //System.out.println("G:");
+        int g = sc.nextInt();
+        //System.out.println("B:");
+        int b = sc.nextInt();
+
+        //CMYK convert
+        int c = 255 - r;
+        int m = 255 - g;
+        int y = 255 - b;
+
+        int k = min(c, m, y);
+        c -= k;
+        m -= k;
+        y -= k;
+
+        System.out.println(c + " " + m + " " + y + " " + k);
+
+    }
+
+    public static int min(int c, int m, int y) {
+        if (c < m && c < y) {
+            return c;
+        } else if (m < c && m < y) {
+            return m;
+        } else {
+            return y;
+        }
+    }
+}
